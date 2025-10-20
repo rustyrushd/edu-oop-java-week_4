ded39ee7089c6ef5dcef7be1b9a2caa95f18b4a3
rgm <git@rushd.xyz>
2025-10-20 13:17:24 +0100

    Input student details and pass to constructor

--- src/main/java/ie/atu/Student.java
+++ src/main/java/ie/atu/Student.java
@@ -1,4 +1,15 @@
 package ie.atu;
 
-public class Student {
+public class Student
+{
+    private String studentName;
+    private String studentEmail;
+    private String studentId;
+
+    public Student(String stName, String stEmail, String stId)
+    {
+        this.studentName = stName;
+        this.studentEmail = stEmail;
+        this.studentId = stId;
+    }
 }
