a7e21250604ba5cbd4bc6e4d565f30f3a528433a
rgm <git@rushd.xyz>
2025-10-20 13:54:44 +0100

    Input loop for user determined number of entries. Each entry is added to a new object and then stored in an arraylist. After final input is completed, the arraylist is printed

--- src/main/java/ie/atu/StudentApp.java
+++ src/main/java/ie/atu/StudentApp.java
@@ -11,17 +11,23 @@ public class StudentApp
         List<Student> studentList = new ArrayList<>();
 
         Scanner scan1 = new Scanner(System.in);
-        System.out.println("Please enter Student name: ");
-        String name = scan1.nextLine();
-        System.out.println("Please enter Student email");
-        String email = scan1.nextLine();
-        System.out.println("Please enter Student ID");
-        String id = scan1.nextLine();
+        System.out.println("Please enter the number of students: ");
+        int count = scan1.nextInt();
+        scan1.nextLine();
+
+        // prompt user count times for input
+        for (int i = 0; i < count; i++) {
+            System.out.println("Please enter Student name: ");
+            String name = scan1.nextLine();
+            System.out.println("Please enter Student email");
+            String email = scan1.nextLine();
+            System.out.println("Please enter Student ID");
+            String id = scan1.nextLine();
+            Student student1 = new Student(name, email, id);
+            studentList.add(student1);
+        }
         scan1.close();
 
-        Student student1 = new Student(name, email, id);
-        studentList.add(student1);
-        System.out.println(student1);
         System.out.println(studentList);
     }
 }
